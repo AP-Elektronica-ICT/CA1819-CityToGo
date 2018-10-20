7c3349a2b03969dabfd2298b6dc68face7d80d88
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-10-20 18:17:23 +0200

    chanche index.js to app

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import Login from './src/components/Login';
-import Router from './src/components/Router';
 
- AppRegistry.registerComponent('newTestNative', () => Router);
+ AppRegistry.registerComponent('newTestNative', () => App);
 
