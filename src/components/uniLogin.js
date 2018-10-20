80ec9352ab955fe89fad0cd0f8246099c4e340f7
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-10-20 17:06:28 +0200

    New login implementation

--- src/components/uniLogin.js
+++ src/components/uniLogin.js
@@ -1,92 +1,140 @@
-/**
- * Auth0Sample 00-Login
- * https://github.com/auth0/react-native-auth0
- * @flow
- */
+import React, { Component } from "react";
+import { View, Text, Button, ActivityIndicator } from "react-native";
+import { NavigationActions, StackActions } from "react-navigation";
+import Auth0 from "react-native-auth0";
+import Config from "react-native-config";
+import DeviceInfo from "react-native-device-info";
+import SInfo from "react-native-sensitive-info";
 
-import React, { Component } from 'react';
+import RNRestart from "react-native-restart";
+
 import {
-  Alert,
-  AppRegistry,
-  Button,
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import Auth0 from 'react-native-auth0';
-import { Actions } from 'react-native-router-flux';
+  headerColorStyle,
+  headerTextColorStyle,
+  buttonStyle
+} from "../../Styles/colors";
+import styles from "../../Styles/Login";
 
-var credentials = require('./auth0-credentials');
-const auth0 = new Auth0(credentials);
+const auth0 = new Auth0({
+  domain: "shakir01.eu.auth0.com",
+  clientId: "1b5iyvAzLoy2GKGYbhXaeGcYRbyDIKn8"
+});
 
-export default class Auth0Sample extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { accessToken: null };
-  }
+export default class uniLogin extends Component {
+  
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: "Login",
+      headerStyle: {
+        backgroundColor: headerColorStyle
+      },
+      headerTitleStyle: {
+        color: headerTextColorStyle
+      }
+    };
+  };
+
+  state = {
+    hasInitialized: false
+  };
 
   componentDidMount() {
-    this._onLogin();
+    
+    console.log("boom panes!");
+    
+    SInfo.getItem("accessToken", {}).then(accessToken => {
+      if (accessToken) {
+        auth0.auth
+          .userInfo({ token: accessToken })
+          .then(data => {
+            this.gotoAccount(data);
+          })
+          .catch(err => {
+            
+            SInfo.getItem("refreshToken", {}).then(refreshToken => {
+              auth0.auth
+                .refreshToken({ refreshToken: refreshToken })
+                .then(newAccessToken => {
+                  SInfo.setItem("accessToken", newAccessToken);
+                  RNRestart.Restart();
+                })
+                .catch(err2 => {
+                  console.log("err getting new access token");
+                  console.log(err2);
+                });
+            });
+          });
+      } else {
+        
+        this.setState({
+          hasInitialized: true
+        });
+        console.log("no access token");
+      }
+    });
   }
-
-  _onLogin = () => {
-    auth0.webAuth
-      .authorize({
-        scope: 'openid profile',
-        audience: 'https://' + credentials.domain + '/userinfo'
-      })
-      .then(credentials => {
-        // Alert.alert(
-        //   'Success',
-        //   'AccessToken: ' + credentials.accessToken,
-        //   [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
-        //   { cancelable: false }
-        // );
-        this.setState({ accessToken: credentials.accessToken });
-        Actions.MainScreen()
-      })
-      .catch(error => console.log(error));
-  };
-
-  _onLogout = () => {
-    if (Platform.OS === 'android') {
-      this.setState({ accessToken: null });
-    } else {
-      auth0.webAuth
-        .clearSession({})
-        .then(success => {
-          this.setState({ accessToken: null });
-        })
-        .catch(error => console.log(error));
-    }
-  };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.header}>Auth0Sample - Login</Text>
-        <Button
-          onPress={this._onLogin}
-          title={'Log In'}
+        <ActivityIndicator
+          size="large"
+          color="#05a5d1"
+          animating={!this.state.hasInitialized}
         />
+        {this.state.hasInitialized && (
+          <Button onPress={this.login} title="Login" color={buttonStyle} />
+        )}
       </View>
     );
   }
+
+  login = () => {
+    auth0.webAuth
+      .authorize({
+        scope: Config.AUTHO_SCOPE,
+        audience: Config.AUTH0_AUDIENCE,
+        device: DeviceInfo.getUniqueID(),
+        prompt: "login"
+      })
+      .then(res => {
+        auth0.auth
+          .userInfo({ token: res.accessToken })
+          .then(data => {
+            this.gotoAccount(data);
+          })
+          .catch(err => {
+            console.log("err: ");
+            console.log(JSON.stringify(err));
+          });
+
+        SInfo.setItem("accessToken", res.accessToken, {});
+        SInfo.setItem("refreshToken", res.refreshToken, {});
+      })
+      .catch(error => {
+        console.log("error occurrdzz");
+        console.log(error);
+      });
+  };
+  
+  gotoAccount = data => {
+    
+    this.setState({
+      hasInitialized: true
+    });
+
+    const resetAction = StackActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({
+          routeName: "MainScreen",
+          params: {
+            name: data.name,
+            picture: data.picture
+          }
+        })
+      ]
+    });
+    this.props.navigation.dispatch(resetAction);
+  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  header: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
-
-
