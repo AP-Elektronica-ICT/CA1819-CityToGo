d20a36bd89e530cb4348dbf048b821b84194affe
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-10-26 15:57:22 +0200

    Auth with token implemented

--- CityToGo_server/server.js
+++ CityToGo_server/server.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
+var port = process.env.PORT || 3000;
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://citytogo.eu.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'http://localhost:3000/',
+    issuer: "https://citytogo.eu.auth0.com/",
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+//debugger
 app.get('/', (req, res) => {
-    console.log(req),
+    //console.log(req),
     res.send('Hello World!') 
+    //debugger
 })
 
 
