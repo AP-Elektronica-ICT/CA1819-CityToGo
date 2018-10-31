cc76d64141be8ea2efec872862dcb655cec76723
Shakir Pathman <pathmanshakir@gmail.com>
2018-10-31 22:35:46 +0100

    Trying to fetch data

--- CityToGo app/src/screens/Profile.js
+++ CityToGo app/src/screens/Profile.js
@@ -4,8 +4,33 @@ import {
     Text,
     StyleSheet
 } from "react-native";
+import Auth0 from "react-native-auth0";
 
+const auth0 = new Auth0({
+    domain: "shakir01.eu.auth0.com",
+    clientId: "1b5iyvAzLoy2GKGYbhXaeGcYRbyDIKn8"
+  });
+var token;
 class Profiel extends Component {
+    
+
+  componentDidMount(){
+    auth0.webAuth.authorize({scope: 'openid profile email', audience: 'https://shakir01.eu.auth0.com/userinfo'})
+    .then((credentials) => {console.log(credentials);token=credentials.accessToken}
+      
+     )
+     console.log(token)
+      let response = fetch('https://shakir01.eu.auth0.com/userinfo', {
+         method: 'GET',
+         headers: {
+          'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           Authorization: 'Bearer ' + token,
+         },
+       });
+       console.log(response);
+       debugger;
+    }
     render() {
         return (
             <View style={styles.container}>
@@ -13,6 +38,7 @@ class Profiel extends Component {
             </View>
         );
     }
+    
 }
 export default Profiel;
 
