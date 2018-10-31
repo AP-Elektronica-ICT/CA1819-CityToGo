c8dc0706c9dfdc8429aaf59f5080bcc6e9d37517
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-10-31 17:42:35 +0100

    Issue with navigation solved

--- CityToGo app/src/screens/Home.js
+++ CityToGo app/src/screens/Home.js
@@ -7,48 +7,49 @@ import {
 
 import Maps from "./Maps";
 import Profile from "./Profile";
-import {Button} from "./../common/Button";
+import { Button } from "./../common/Button";
 import { NavigationActions, StackActions } from "react-navigation";
 
 
 class Home extends Component {
-    onButtonPress(){
-        const resetAction = StackActions.reset({
-            index: 0,
-            
-            actions: [
-              
-              NavigationActions.navigate({
-                routeName: "Profile",
-                
-                params: {}
-              })
-            ]
-            
-          });
-          this.props.navigation.dispatch(resetAction);
-    
-    };
+    // onButtonPress() {
+    //     const resetAction = StackActions.reset({
+    //         index: 0,
+
+    //         actions: [
+
+    //             NavigationActions.navigate({
+    //                 routeName: "Profile",
+
+    //                 params: {}
+    //             })
+    //         ]
+
+    //     });
+    //     this.props.navigation.dispatch(resetAction);
+
+    // };
 
     render() {
+        const { navigate } = this.props.navigation;
         return (
-            
+
             // <View style={styles.container} >
             //   <Button>Profile</Button>
             //     <Maps />
             // </View>
             <View style={{ flex: 1 }}>
-             <Maps/>
-             <View style={{
-            position: 'absolute',//use absolute position to show button on top of the map
-            top: '2%', //for center align
-            alignSelf: 'flex-end' //for align to right
-         }}>
-        <Button onPress={()=>this.onButtonPress()}>Profile</Button>
-    </View>
+                <Maps />
+                <View style={{
+                    position: 'absolute',//use absolute position to show button on top of the map
+                    top: '2%', //for center align
+                    alignSelf: 'flex-end' //for align to right
+                }}>
+                    <Button onPress={() => navigate('Profile', { name: 'Jane' })}>Profile</Button>
+                </View>
             </View>
         );
-        
+
     }
 }
 export default Home;
