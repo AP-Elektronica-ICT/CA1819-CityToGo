bbbbf89c7fbcc75ee93a252fab36300932e6552f
Shakir Pathman <pathmanshakir@gmail.com>
2018-11-01 17:17:35 +0100

    Profile page created, Getting error with metadata

--- CityToGo app/src/screens/Profile.js
+++ CityToGo app/src/screens/Profile.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+     Image,
 } from "react-native";
 import Auth0 from "react-native-auth0";
 
@@ -30,27 +31,124 @@ class Profiel extends Component {
                 Authorization: 'Bearer ' + token,
             },
         }).then((response) => response.json())
-            .then(responseJson => data = responseJson).then(console.log(data));
+            .then(responseJson => data = responseJson).then(console.log(data.nickname));
 
 
-
-        console.log(data);  
+        const  metadata = data["https://shakir01.eu.auth0.com/user_metadata"]
+        console.log(metadata);  
+    
     }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Profle</Text>    
-            </View>
+          <View style={styles.container}>
+              <View style={styles.header}>
+                <View style={styles.headerContent}>
+                    <Image style={styles.avatar}
+                      source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+    
+                    <Text style={styles.name}>{data.email} </Text>
+                    <Text style={styles.userInfo}>{data.email}</Text>
+                    <Text style={styles.userInfo}> {data.nickname} </Text>
+                </View>
+              </View>
+    
+              <View style={styles.body}>
+                <View style={styles.item}>
+                  <View style={styles.iconContent}>
+                    <Image style={styles.icon} source={{uri: 'https://png.icons8.com/home/win8/50/ffffff'}}/>
+                  </View>
+                  <View style={styles.infoContent}>
+                    <Text style={styles.info}>Home</Text>
+                  </View>
+                </View>
+    
+                <View style={styles.item}>
+                  <View style={styles.iconContent}>
+                    <Image style={styles.icon} source={{uri: 'https://png.icons8.com/settings/win8/50/ffffff'}}/>
+                  </View>
+                  <View style={styles.infoContent}>
+                    <Text style={styles.info}>Settings</Text>
+                  </View>
+                </View>
+    
+                <View style={styles.item}>
+                  <View style={styles.iconContent}>
+                    <Image style={styles.icon} source={{uri: 'https://png.icons8.com/news/win8/50/ffffff'}}/>
+                  </View>
+                  <View style={styles.infoContent}>
+                    <Text style={styles.info}>News</Text>
+                  </View>
+                </View>
+    
+                <View style={styles.item}>
+                  <View style={styles.iconContent}>
+                    <Image style={styles.icon} source={{uri: 'https://png.icons8.com/shopping-basket/ios11/50/ffffff'}}/>
+                  </View>
+                  <View style={styles.infoContent}>
+                    <Text style={styles.info}>Shop</Text>
+                  </View>
+                </View>
+    
+              </View>
+          </View>
         );
+      }
     }
-
-}
-export default Profiel;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    
+    const styles = StyleSheet.create({
+      header:{
+        backgroundColor: "#DCDCDC",
+      },
+      headerContent:{
+        padding:30,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
+      },
+      avatar: {
+        width: 130,
+        height: 130,
+        borderRadius: 63,
+        borderWidth: 4,
+        borderColor: "white",
+        marginBottom:10,
+      },
+      name:{
+        fontSize:22,
+        color:"#000000",
+        fontWeight:'600',
+      },
+      userInfo:{
+        fontSize:16,
+        color:"#778899",
+        fontWeight:'600',
+      },
+      body:{
+        backgroundColor: "#778899",
+        height:500,
+        alignItems:'center',
+      },
+      item:{
+        flexDirection : 'row',
+      },
+      infoContent:{
+        flex:1,
+        alignItems:'flex-start',
+        paddingLeft:5
+      },
+      iconContent:{
+        flex:1,
+        alignItems:'flex-end',
+        paddingRight:5,
+      },
+      icon:{
+        width:30,
+        height:30,
+        marginTop:20,
+      },
+      info:{
+        fontSize:18,
+        marginTop:20,
+        color: "#FFFFFF",
+      }
+    });
+    export default Profiel;
