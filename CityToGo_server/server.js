030e352dac405171e800270c459f1b3b2d113482
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-11-07 22:34:12 +0100

    master backend implementation

--- CityToGo_server/server.js
+++ CityToGo_server/server.js
@@ -3,6 +3,11 @@ const app = express()
 var jwt = require('express-jwt');
 var jwks = require('jwks-rsa');
 const fetch = require('node-fetch');
+const bodyparser = require('body-parser');
+const haversine = require('haversine');
+
+
+app.use(bodyparser.json());
 
 var port = process.env.PORT || 3000;
 
@@ -19,13 +24,20 @@ var jwtCheck = jwt({
 });
 
 //Disabled jwl token to prevent unauthorized request 
-app.use(jwtCheck);
+//app.use(jwtCheck);
+let arr= [];
 
-
+//Haalt alle data van opendata api
 app.get('/api/monumenten', (req, res) => {
     fetch('https://opendata.arcgis.com/datasets/628ded9e05184e76b69719eb8ce0e0aa_207.geojson')
         .then(res => res.json())
-        .then(json => res.send(json.features) )
+        .then(json => {res.send(json.features); arr=[];  json.features.forEach(element => {
+    
+            // pusht data van api in een variable
+            arr.push(element);
+
+            
+        }); } )
         .catch(err=>console.error(err));
 })
 
@@ -33,4 +45,69 @@ app.get('/api/monumenten', (req, res) => {
 
 
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+
+
+
+let currentUserLocation = {
+    
+    latitude: "",
+    longitude: ""
+}
+
+
+
+//berekent afstand tussen een monument en de huidige locatie
+function calculateLocation(locationUser,locationDest){
+    
+    let LocationDS ={
+        latitude: locationDest.geometry.coordinates[0][0][0],
+        longitude: locationDest.geometry.coordinates[0][0][1]
+            }
+
+//Distance wordt in de array gestoken
+    locationDest.geometry.coordinates[0][0][2]=(haversine(currentUserLocation,LocationDS));
+    console.log(locationDest.geometry.coordinates[0][0][2]);
+    
+
+}
+
+// Huidige locatie wordt hier gegeven
+app.post('/api/getNextLocation', (req, res) => {
+    
+    currentUserLocation.latitude = req.body.latitude;
+    currentUserLocation.longitude =req.body.longitude;
+    
+    // voor elke coordinaat van elke monument wordt de afstand berekend tov de huidige locatie
+    arr.forEach(element => {
+        calculateLocation(currentUserLocation,element);
+    });
+
+
+
+
+
+let shortest=arr[0];
+
+
+// Elke element van de array wordt vergeleken met de huidige dichtbijzijndste locatie
+arr.forEach(element=>{
+
+    if(element.geometry.coordinates[0][0][2] < shortest.geometry.coordinates[0][0][2]){
+        shortest=[];
+        shortest = element;
+
+
+    }
+
+});
+res.json(shortest);
+
+}
+);
+
+app.listen(port, () =>console.log(`Example app listening on port ${port}!`));
+
+
+
+
+
