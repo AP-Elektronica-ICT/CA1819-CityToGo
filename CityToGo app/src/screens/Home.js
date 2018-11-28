494b45d970f3f47c352086665506ecf8c4ffa1a9
Shakir Pathman <pathmanshakir@gmail.com>
2018-11-28 10:38:52 +0100

    komma error opgelost na merge

--- CityToGo app/src/screens/Home.js
+++ CityToGo app/src/screens/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
             polygons: [],
             visible: false,
             data:"",
-            Name:""
+            Name:"",
             polygons: [],
             randomQuizes: [],
             randomNumber:0
