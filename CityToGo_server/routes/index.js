3867cdcaa52957dfb159652aea0f0eb6eda98ab9
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-12-09 15:15:45 +0100

    add some routing

--- CityToGo_server/routes/index.js
+++ CityToGo_server/routes/index.js
@@ -1,17 +1,14 @@
 express = require('express');
 var router = express.Router();
 
-var userSession_controller = require('../controllers/userData')
+var userSession_controller = require('../controllers/userSessionController')
 
 /*
  * Routes that can be accessed by anyone (temporally)
  */
 router.get('/api/v1/userData/test', userSession_controller.test);
-router.post('/api/v1/userData/create', userSession_controller.session_create);
-
-// router.get('/api/v1/product/:id', products.getOne);
-// router.post('/api/v1/product/', products.create);
-// router.put('/api/v1/product/:id', products.update);
-// router.delete('/api/v1/product/:id', products.delete);
+router.get('/api/v1/userSession/find/:userId', userSession_controller.session_find)
+router.post('/api/v1/userSession/create', userSession_controller.session_create);
+router.put('/api/v1/userSession/update/:id', userSession_controller.session_update)
 
 module.exports = router;
