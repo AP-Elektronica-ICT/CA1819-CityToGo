e6c18990ec94946d3666fb1e33551fa3fab23b56
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-12-19 16:31:01 +0100

    create and test portal AR component

---
+++ CityToGo app/src/components/PortalAR.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import React, { Component } from "react";
+
+import {
+    ViroSceneNavigator,
+    ViroScene,
+    ViroARScene,
+    ViroAmbientLight,
+    Viro360Video,
+    Viro360Image,
+    ViroUtils,
+    ViroPortal,
+    ViroPortalScene,
+    Viro3DObject,
+} from 'react-viro';
+
+// var arFiles = {
+//     'portal_ship_vrx': require('/portal_ship/portal_ship.vrx'),
+// }
+
+export default class PortalAR extends Component {
+    render() {
+        return (
+            <ViroARScene>
+                <ViroAmbientLight color="#ffffff" intensity={200} />
+                <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={() => { }}>
+                    <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
+                        <Viro3DObject source={require('./portal_ship/portal_ship.vrx')}
+                            resources={[require('./portal_ship/portal_ship_diffuse.png'),
+                            require('./portal_ship/portal_ship_normal.png'),
+                            require('./portal_ship/portal_ship_specular.png')]}
+                            type="VRX" />
+                    </ViroPortal>
+                    <Viro360Image source={require( "./portal_ship/360_island.jpg")} />
+                </ViroPortalScene>
+            </ViroARScene>
+        );
+
+
+    }
+}
+module.exports = PortalAR;
