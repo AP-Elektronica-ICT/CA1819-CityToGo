c17f0c9596c01f918c0718261de00a6081c9903e
Mohammed L <mohammed.laghzaoui@outlook.com>
2019-01-04 02:31:45 +0100

    create post reducer and action

--- CityToGo app/src/redux/actions/userSessionAction.js
+++ CityToGo app/src/redux/actions/userSessionAction.js
@@ -1,12 +1,40 @@
-import { FETCHING_GET_USER_SESSION } from "./types";
+import {
+    FETCHING_GET_USER_SESSION,
+    FETCHING_POST_USER_SESSION,
+    FETCHING_PUT_USER_SESSION
+} from "./types";
+
 import Config from '../../config/config'
 import axios from "axios";
 
-export function userSession(userID) {
+export function getUserSession(userID) {
     return {
         type: FETCHING_GET_USER_SESSION,
         payload: axios.get(`http://${Config.MY_IP_ADRES}:3000/api/v1/userSession/find/${userID}`)
-            .then((res) => res)
+            .then((res) => res.data)
     }
 }
 
+export function postUserSession(userId, isRunning, startTime, stopTime, isFound, monument, ) {
+    let body = JSON.stringify({
+        userId: userId,
+        isRunning: isRunning,
+        subSession: {
+            startTime: startTime,
+            stopTime: stopTime,
+            isFound: isFound,
+            monument: monument
+        }
+    })
+
+    return {
+        type: FETCHING_POST_USER_SESSION,
+        payload: axios.post(`http://${Config.MY_IP_ADRES}:3000/api/v1/userSession/create`, body, {
+            headers: {
+                // authorization: 'Bearer ' + global.token,
+                // Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }
+        }).then(res => res.data)
+    }
+}
